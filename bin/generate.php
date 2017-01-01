0d063ec01a161af6ac4c6c555ce13728f2260e90
widmogrod <widmogrod@gmail.com>
2017-01-01 20:55:14 +0100

    Reuse code in bin/ scripts

--- bin/generate.php
+++ bin/generate.php
@@ -1,42 +1,10 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$dto = new RunTheDocs\DTO\GroupOfExamples(
-    'ExampleOfEitherMonadTest',
-    'ExampleOfEitherMonadTest',
-    'In php world, the most popular way of saying that something went wrong is to throw an exception.
-This results in nasty try catch blocks and many of if statements.
-
-Either Monad shows how we can fail gracefully without breaking the execution chain and making the code more readable.',
-    [
-        new \RunTheDocs\DTO\Example(
-            'test_example_how_array_map_can_be_used',
-            'test_example_how_array_map_can_be_used',
-            'The following example demonstrates combining the contents of two files into one.
-If one of those files does not exist the operation fails gracefully.',
-            '
-        $read = function ($file) {
-            return is_file($file)
-                ? Either\Right::of(file_get_contents($file))
-                : Either\Left::of(sprintf(\'File "%s" does not exists\', $file));
-        };
-
-        $concat = f\liftM2(
-            $read(__FILE__),
-            $read(\'./this-file-does-not-exits\'),
-            function ($first, $second) {
-                return $first . $second;
-            }
-        );
-
-        assert($concat instanceof Either\Left);
-        assert($concat->extract() === \'File "./this-file-does-not-exits" does not exists\');
-    '
-        )
-    ]
-
-);
-
+$extractor = new \RunTheDocsPHPUnit\PhpunitExtractor();
+$dto = $extractor->extract(new \RunTheDocs\Extractor\ValueObject\File(
+    __DIR__ . '/../example/ExampleOfEitherMonadTest.php'
+));
 
 $generator = new \RunTheDocs\Generator\Markdown\Markdown();
 $result = $generator->generate($dto);
